5e7ead6e42f5cbc1e56df2628e982b81fb9522a6
Raphaël Carabeuf <raph.carabeuf@gmail.com>
2023-05-10 15:44:11 +0200

    Intro created

--- src/main/java/com/example/harry_potter_javafx/HelloApplication.java
+++ src/main/java/com/example/harry_potter_javafx/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.harry_potter_javafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +11,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start-view.fxml"));
+        StackPane root = fxmlLoader.load();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Harry Potter at Home");
         stage.setScene(scene);
+        stage.setFullScreen(true); // Full screen
         stage.show();
     }
 
